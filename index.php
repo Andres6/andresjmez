6460ed755cb640461f5b90104fc1c87932361a30
Andres <andresjmez@gmail.com>
2016-11-30 11:58:26 -0500

    footer fix

--- index.php
+++ index.php
@@ -122,8 +122,8 @@
 			<div class="footer_name"> 
 				<div class="social_icons"> 
 					<ul id="social_list"> 
-						<li><a href="https://github.com/Andres6" target="_blank" title="GitHub"><img src="../site/media/github_white.png" alt="GitHub" id="footer_github"/></a></li>
-						<li><a href="../contact/" title="Contact Me"><img src="../site/media/mail.png" alt="Contact Me" id="footer_contact"/></a></li>
+						<li><a href="https://github.com/Andres6" target="_blank" title="GitHub"><img src="site/media/github_white.png" alt="GitHub" id="footer_github"/></a></li>
+						<li><a href="contact/" title="Contact Me"><img src="site/media/mail.png" alt="Contact Me" id="footer_contact"/></a></li>
 					</ul>
 				</div> 
 				<p>Website designed and built by Andres Jimenez</p> 
