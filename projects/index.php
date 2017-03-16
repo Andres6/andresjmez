1881d755e51ff73a9dcfabb8609131aeab0e4a67
Andres <andresjmez@gmail.com>
2017-03-16 16:47:46 -0400

    mobile menu login added

--- projects/index.php
+++ projects/index.php
@@ -35,6 +35,7 @@
 					<li><a href="../about/" id="aboutNav">About</a></li>
 					<li><a href="../contact/" id="contactNav">Contact</a></li>
 					<li><a href="../projects/" id="projectsNav">Projects</a></li>
+					<li id="loginLink"><a href="login/">Login</a></li>
 				</ul>
 			</div>
 
