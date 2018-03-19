0d82086cc63142e77551a71fc1c7821842c84fd7
Andres Jimenez <andresjmez@gmail.com>
2018-03-19 10:20:27 -0400

    testing GET

--- getresponse/index.php
+++ getresponse/index.php
@@ -1,8 +1,12 @@
 <?php
 header("HTTP/1.1 200 OK");
-header("Content-Type:plain/text");
-foreach ($_REQUEST as $value) {
+
+if ('PingPlease return OK so that I know your service works.'){
+	echo "OK";
+} else {
+	foreach ($_REQUEST as $value) {
 	echo $value;
+}
 }
 
 ?>
