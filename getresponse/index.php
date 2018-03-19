7be8cd2c63b95ca83d299dfb977921602fc7f05e
Andres Jimenez <andresjmez@gmail.com>
2018-03-19 11:45:52 -0400

    testing GET

--- getresponse/index.php
+++ getresponse/index.php
@@ -6,6 +6,33 @@
 		else if ($value === 'Name'){
 			echo "Bobby Bob";
 		}
+		else if ($value === 'Position'){
+			echo "Mega Super Programmer";
+		}
+		else if ($value === 'Resume'){
+			echo "BobbyBobsResume.com";
+		}
+		else if ($value === 'Source'){
+			echo "BobbyBobResume/download";
+		}
+		else if ($value === 'Status'){
+			echo "Yes";
+		}
+		else if ($value === 'Years'){
+			echo "300+";
+		}
+		else if ($value === 'Referrer'){
+			echo "Billy";
+		}
+		else if ($value === 'Degree'){
+			echo "Phd in Mega Programming";
+		}
+		else if ($value === 'Email Address'){
+			echo "Bobby@Bob.com";
+		}
+		else if ($value === 'Phone'){
+			echo "+1 (111) 111-1111";
+		}
 		else {
 			error_log($value);
 		} 
