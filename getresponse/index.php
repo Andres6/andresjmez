5f964e1150d9799e25af147cd36300c5dec0b53a
Andres Jimenez <andresjmez@gmail.com>
2018-03-19 10:38:12 -0400

    testing GET

--- getresponse/index.php
+++ getresponse/index.php
@@ -1,12 +1,3 @@
 <?php
 header("HTTP/1.1 200 OK");
-
-if ('PingPlease return OK so that I know your service works.'){
-	echo "OK";
-} else {
-	foreach ($_REQUEST as $value) {
-	echo $value;
-}
-}
-
 ?>
