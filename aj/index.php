df758d416c44dfcce4364504bc7374fa601c2495
AJ <andresjmez@gmail.com>
2020-08-07 23:22:25 -0400

    new site under aj in development- home page test

---
+++ aj/index.php
@@ -0,0 +1,79 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- bootstrap -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+		<link rel="stylesheet" href="./aj.css">
+		<link rel="icon" href="../../favicon.ico" type="image/x-icon">
+
+		<title>Pua</title>
+
+		<!-- Google Analytics -->
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-73977107-1', 'auto');
+			ga('send', 'pageview');
+		</script>
+	</head>
+
+	<body id="projects">    
+		<!-- Full Site -->
+		<div class="wrapper container-fluid px-0">
+			<!-- Nav Bar -->
+			<header class="navbar navbar-expand-lg navbar-light">
+				<div class="container px-0">
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+					
+					<a class="navbar-brand" href="../../">andresjmez</a>
+					<div class="collapse navbar-collapse" id="navbarToggler">
+						<ul class = "navbar-nav ml-auto mr-2 mt-2 mt-lg-0" id="top-menu">
+							<li class="nav-item"><a href="../../" class="nav-link acti" id="homeNav">Home</a></li>
+							<li class="nav-item"><a href="../../about/" class="nav-link" id="aboutNav">About</a></li>
+							<li class="nav-item"><a href="../../contact/" class="nav-link" id="contactNav">Contact</a></li>
+							<li class="nav-item"><a href="../../projects/" class="nav-link" id="projectsNav">Projects</a></li>
+						</ul>
+					</div>
+				</div>
+			</header>
+
+
+            <!-- Main Content of Page -->
+            <section class="container-fluid welcome">
+                <div class="container-lg content">
+                    <div class="row">
+                        <div class="col-lg-5 align-self-center">
+                            <div class=" text-center">
+                                <h3 class="display-4 text-light font-weight-bold">Andres Jimenez</h3>
+                                <p class="display-5 text-light font-weight-bold">Hello World! I'm a software engineer, welcome.</p>
+                            </div>
+                        </div>
+                        <div class="col-lg-7">
+                            <div class="text-center">
+                                <figure class="figure mt-3 mb-0">
+                                    <img src="./roundMeGrey.png" class="me figure-img img-fluid mb-0" alt="Stupid Pig">
+                                    <figcaption class="figure-caption font-italic text-right">It's me</figcaption>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+			<svg viewBox="0 0 1000 80" preserveAspectRatio="none">
+ 				<path class="svgMain" d="m 0 0 l 0 40 q 80 50 350 0 q 270 -50 650 30 l 1000 -80 z " fill="#a0d0e4" />
+			</svg>
+		</div>
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+	</body>
+</html>
