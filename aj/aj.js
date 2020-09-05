5b3d4394650915369de585d881025665840cb704
AJ <andresjmez@gmail.com>
2020-09-05 18:37:35 -0400

    testing server and new site

---
+++ aj/aj.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('.second-button').on('click', function() {
+        $('.animated-icon2').toggleClass('open');
+    });
+});
--- aj/index.php
+++ aj/index.php
@@ -1,85 +1,305 @@
-
 <!DOCTYPE html>
 <html lang="en">
-	<head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-		<!-- bootstrap -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-		<link rel="stylesheet" href="./aj.css">
-		<link rel="icon" href="../../favicon.ico" type="image/x-icon">
 
-		<link href="https://fonts.googleapis.com/css2?family=Srisakdi:wght@700&display=swap" rel="stylesheet">
-		<link href="https://fonts.googleapis.com/css2?family=Contrail+One&family=Handlee&family=Poiret+One&family=Roboto+Mono&display=swap" rel="stylesheet">
-		<title>Pua</title>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- bootstrap -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
-		<!-- Google Analytics -->
-		<script>
-			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+    <link rel="icon" href="../../favicon.ico" type="image/x-icon">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <link href="https://fonts.googleapis.com/css2?family=Srisakdi:wght@700&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Contrail+One&family=Handlee&family=Poiret+One&family=Roboto+Mono&display=swap"
+        rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./aj.css">
+    <title>Pua</title>
 
-			ga('create', 'UA-73977107-1', 'auto');
-			ga('send', 'pageview');
-		</script>
-	</head>
+    <!-- Google Analytics -->
+    <script>
+    (function(i, s, o, g, r, a, m) {
+        i['GoogleAnalyticsObject'] = r;
+        i[r] = i[r] || function() {
+            (i[r].q = i[r].q || []).push(arguments)
+        }, i[r].l = 1 * new Date();
+        a = s.createElement(o),
+            m = s.getElementsByTagName(o)[0];
+        a.async = 1;
+        a.src = g;
+        m.parentNode.insertBefore(a, m)
+    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-	<body id="projects">    
-		<!-- Full Site -->
-		<div class="wrapper container-fluid px-0">
-			<!-- Nav Bar -->
-			<header class="navbar navbar-expand-lg navbar-light">
-				<div class="container px-0">
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-						<span class="navbar-toggler-icon"></span>
-					</button>
-					
-					<a class="navbar-brand" href="../../">andresjmez</a>
-					<div class="collapse navbar-collapse" id="navbarToggler">
-						<ul class = "navbar-nav ml-auto mr-2 mt-2 mt-lg-0" id="top-menu">
-							<li class="nav-item"><a href="../../" class="nav-link acti" id="homeNav">Home</a></li>
-							<li class="nav-item"><a href="../../about/" class="nav-link" id="aboutNav">About</a></li>
-							<li class="nav-item"><a href="../../contact/" class="nav-link" id="contactNav">Contact</a></li>
-							<li class="nav-item"><a href="../../projects/" class="nav-link" id="projectsNav">Projects</a></li>
-						</ul>
-					</div>
-				</div>
-			</header>
+    ga('create', 'UA-73977107-1', 'auto');
+    ga('send', 'pageview');
+    </script>
+</head>
+
+<body id="projects">
+    <!-- Full Site -->
+    <div class="wrapper container-fluid px-0">
+        <!-- Nav Bar -->
+        <header class="navbar navbar-expand-lg">
+            <div class="container px-0">
+                <button class="navbar-toggler second-button" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent23" aria-controls="navbarSupportedContent23"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <div class="animated-icon2"><span></span><span></span><span></span><span></span></div>
+                </button>
+
+                <a class="navbar-brand" href="../../">
+                    <img src="./blobbang_spiral.png" height="50" class="mylogo" alt="">
+                    <span class="brandname_first">andres</span><span class="brandname_last">jmez</span>
+                </a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent23">
+                    <ul class="navbar-nav ml-auto mr-2 mt-2 mt-lg-0" id="top-menu">
+                        <li class="nav-item"><a href="../../" class="nav-link acti" id="homeNav">home</a></li>
+                        <li class="nav-item"><a href="../../about/" class="nav-link" id="aboutNav">about</a></li>
+                        <li class="nav-item"><a href="../../contact/" class="nav-link" id="contactNav">contact</a></li>
+                        <li class="nav-item"><a href="../../projects/" class="nav-link" id="projectsNav">projects</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </header>
 
 
-            <!-- Main Content of Page -->
-            <section class="container-fluid welcome">
-                <div class="container-lg content">
-                    <div class="row">
-                        <div class="col-lg-5 align-self-center">
-                            <div class="intro text-center">
-								<h3 class="display-3 text-light font-weight-bold">I'm Andres.</h3>
-								<h4 class="display-5 text-light font-weight-bold">***</h4>
-                                <h4 class="display-5 text-light font-weight-bold">software engineer, welcome!</h4>
-                            </div>
+        <!-- Main Content of Page -->
+        <section class="container-fluid welcome">
+            <div class="container-lg content">
+                <div class="row">
+                    <div class="col-lg-5 align-self-center">
+                        <div class="intro text-center ">
+                            <h2 class="font-weight-bold">I'm Andres.</h2>
+                            <h4 class="display-5 font-weight-bold"></h4>
+                            <h4 class="h4-responsive font-weight-bold">software engineer, welcome!</h4>
                         </div>
-                        <div class="col-lg-7">
-                            <div class="text-center">
-                                <figure class="figure mt-3 mb-0">
-                                    <img src="./roundMeGrey.png" class="me figure-img img-fluid mb-0" alt="Stupid Pig">
-                                    <figcaption class="figure-caption font-italic text-right">It's me</figcaption>
-                            </div>
+                    </div>
+                    <div class="col-lg-7">
+                        <div class="text-center">
+                            <figure class="figure mt-3 mb-0">
+                                <img src="./binmebr.png" class="me figure-img img-fluid mb-0" alt="Stupid Pig">
+                                <figcaption class="figure-caption font-italic text-right">It's me</figcaption>
                         </div>
                     </div>
                 </div>
-            </section>
-			<svg viewBox="0 0 1000 80" preserveAspectRatio="none">
- 				<path class="svgMain" d="m 0 0 l 0 40 q 80 50 350 0 q 270 -50 650 30 l 1000 -80 z " fill="#92c8de" />
-			</svg>
-			<section class="container">
+            </div>
+        </section>
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#3ebcde" fill-opacity="1"
+                d="M0,288L14.1,261.3C28.2,235,56,181,85,165.3C112.9,149,141,171,169,165.3C197.6,160,226,128,254,138.7C282.4,149,311,203,339,234.7C367.1,267,395,277,424,240C451.8,203,480,117,508,112C536.5,107,565,181,593,186.7C621.2,192,649,128,678,122.7C705.9,117,734,171,762,165.3C790.6,160,819,96,847,106.7C875.3,117,904,203,932,234.7C960,267,988,245,1016,224C1044.7,203,1073,181,1101,154.7C1129.4,128,1158,96,1186,85.3C1214.1,75,1242,85,1271,122.7C1298.8,160,1327,224,1355,250.7C1383.5,277,1412,267,1426,261.3L1440,256L1440,0L1425.9,0C1411.8,0,1384,0,1355,0C1327.1,0,1299,0,1271,0C1242.4,0,1214,0,1186,0C1157.6,0,1129,0,1101,0C1072.9,0,1045,0,1016,0C988.2,0,960,0,932,0C903.5,0,875,0,847,0C818.8,0,791,0,762,0C734.1,0,706,0,678,0C649.4,0,621,0,593,0C564.7,0,536,0,508,0C480,0,452,0,424,0C395.3,0,367,0,339,0C310.6,0,282,0,254,0C225.9,0,198,0,169,0C141.2,0,113,0,85,0C56.5,0,28,0,14,0L0,0Z">
+            </path>
+        </svg>
+        <section class="container text-center">
+            <!-- <div class="shebang">
+                <img class="img-fluid" src="./blob_hi.png" alt="blob">
+            </div> -->
+        </section>
+        <section class="container">
+            <div class="row about-intro">
+                <div class="col-lg-6">
+                    <h3 class="h3-responsive font-weight-bold sect-title text-center">about</h3>
+                    <p class="infotext">I'm a software engineer based around New York, NY. I enjoy
+                        building/designing
+                        applications with
+                        clean and reusable code. I enjoy creating projects across the spectrum from designing
+                        websites(like
+                        this one)
+                        to
+                        building parallel computing simulation software
+                    </p>
+                </div>
+                <div class="col-lg-6"></div>
+            </div>
 
-			</section>
-		</div>
+            <div class="row">
+                <div class="col-lg-4"></div>
+                <div class="col-lg-4 py-5">
+                    <img class="img-fluid shebang" src="./blobbang_spiral.png" alt="blob">
+                </div>
+                <div class="col-4"></div>
+            </div>
 
-        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
-	</body>
+            <div class="row">
+                <div class="col-lg-6"></div>
+                <div class="col-lg-6 text-center">
+                    <h3 class="h3-responsive font-weight-bold sect-title text-center">more about</h3>
+                    <p class="">Name: Andres Jimenez </p>
+                    <p class="">Location: 40°48'08.3"N 73°58'50.1"W </p>
+                    <p class="">Age: 31 </p>
+                </div>
+        </section>
+
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+            <path fill="#2097b7" fill-opacity="1"
+                d="M0,128L12,144C24,160,48,192,72,208C96,224,120,224,144,224C168,224,192,224,216,186.7C240,149,264,75,288,80C312,85,336,171,360,176C384,181,408,107,432,117.3C456,128,480,224,504,245.3C528,267,552,213,576,197.3C600,181,624,203,648,186.7C672,171,696,117,720,122.7C744,128,768,192,792,224C816,256,840,256,864,245.3C888,235,912,213,936,218.7C960,224,984,256,1008,250.7C1032,245,1056,203,1080,160C1104,117,1128,75,1152,64C1176,53,1200,75,1224,112C1248,149,1272,203,1296,197.3C1320,192,1344,128,1368,96C1392,64,1416,64,1428,64L1440,64L1440,320L1428,320C1416,320,1392,320,1368,320C1344,320,1320,320,1296,320C1272,320,1248,320,1224,320C1200,320,1176,320,1152,320C1128,320,1104,320,1080,320C1056,320,1032,320,1008,320C984,320,960,320,936,320C912,320,888,320,864,320C840,320,816,320,792,320C768,320,744,320,720,320C696,320,672,320,648,320C624,320,600,320,576,320C552,320,528,320,504,320C480,320,456,320,432,320C408,320,384,320,360,320C336,320,312,320,288,320C264,320,240,320,216,320C192,320,168,320,144,320C120,320,96,320,72,320C48,320,24,320,12,320L0,320Z">
+            </path>
+        </svg>
+
+
+        <!-- Footer -->
+        <footer class="page-footer font-small pt-4">
+
+            <!-- Footer Links -->
+            <div class="container text-center text-md-left">
+
+                <!-- Footer links -->
+                <div class="row text-center text-md-left mt-3 pb-3">
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">ANDRESJMEZ.COM</h6>
+                        <p>Rows and columns. Lorem ipsum dolor sit
+                            amet,
+                            consectetur
+                            adipisicing elit.</p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none">
+
+                    <!-- Grid column -->
+                    <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Products</h6>
+                        <p>
+                            <a href="#!">Linux Debian</a>
+                        </p>
+                        <p>
+                            <a href="#!">Raspberry Pi</a>
+                        </p>
+                        <p>
+                            <a href="#!">Mpi</a>
+                        </p>
+                        <p>
+                            <a href="#!">Python</a>
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                    <hr class="w-100 clearfix d-md-none">
+
+                    <!-- Grid column -->
+                    <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Useful links</h6>
+                        <p>
+                            <a href="#!">Your Account</a>
+                        </p>
+                        <p>
+                            <a href="#!">Become an Member</a>
+                        </p>
+                        <p>
+                            <a href="#!">About me</a>
+                        </p>
+                        <p>
+                            <a href="#!">Help</a>
+                        </p>
+                    </div>
+
+                    <!-- Grid column -->
+                    <hr class="w-100 clearfix d-md-none">
+
+                    <!-- Grid column -->
+                    <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mt-3">
+                        <h6 class="text-uppercase mb-4 font-weight-bold">Contact</h6>
+                        <p>
+                            <i class="fas fa-home mr-3"></i> New York, NY 10001, US
+                        </p>
+                        <p>
+                            <i class="fas fa-envelope mr-3"></i> andresjmez@gmail.com
+                        </p>
+                        <p>
+                            <i class="fas fa-phone mr-3"></i> + 01 234 567 88
+                        </p>
+                        <p>
+                            <i class="fas fa-print mr-3"></i> + 01 234 567 89
+                        </p>
+                    </div>
+                    <!-- Grid column -->
+
+                </div>
+                <!-- Footer links -->
+
+                <hr>
+
+                <!-- Grid row -->
+                <div class="row d-flex align-items-center">
+
+                    <!-- Grid column -->
+                    <div class="col-md-7 col-lg-8">
+
+                        <!--Copyright-->
+                        <p class="text-center text-md-left">© 2020 Copyright:
+                            <a href="https://andresjmez.com/">
+                                <strong> andresjmez.com</strong>
+                            </a>
+                        </p>
+
+                    </div>
+                    <!-- Grid column -->
+
+                    <!-- Grid column -->
+                    <div class="col-md-5 col-lg-4 ml-lg-0">
+
+                        <!-- Social buttons -->
+                        <div class="text-center text-md-right">
+                            <ul class="list-unstyled list-inline">
+                                <li class="list-inline-item">
+                                    <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                        <i class="fab fa-google-plus-g"></i>
+                                    </a>
+                                </li>
+                                <li class="list-inline-item">
+                                    <a class="btn-floating btn-sm rgba-white-slight mx-1">
+                                        <i class="fab fa-linkedin-in"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+
+                    </div>
+                    <!-- Grid column -->
+
+                </div>
+                <!-- Grid row -->
+
+            </div>
+            <!-- Footer Links -->
+
+        </footer>
+        <!-- Footer -->
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+    </script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous">
+    </script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+    </script>
+    <script src="./aj.js">
+    </script>
+</body>
+
 </html>
