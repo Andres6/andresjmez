9afcbfb815575d9811cf6e2e40202afbe217e1b1
AJ <andresjmez@gmail.com>
2021-12-04 09:56:01 -0500

    updated analytics tag

--- index.php
+++ index.php
@@ -2,6 +2,16 @@
 <html lang="en">
 
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-73977107-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-73977107-1');
+    </script>
+
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -43,23 +53,6 @@
     <meta name="theme-color" content="#ffffff">
 
 
-    <!-- Google Analytics -->
-    <script>
-    (function(i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function() {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-            m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
-
-    ga('create', 'UA-73977107-1', 'auto');
-    ga('send', 'pageview');
-    </script>
 </head>
 
 
@@ -93,9 +86,6 @@
                 </div>
             </div>
         </header>
-
-
-        
 
 
         <!-- Main Content of Page -->
